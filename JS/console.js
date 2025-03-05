1ac737ef77840c42dd3ef79844ec353292c58cc2
Akine2606 <minhducbarret@gmail.com>
2025-03-05 09:45:31 +0700

    uploading

---
+++ JS/console.js
@@ -0,0 +1,28 @@
+function upDate(reviewPic)
+{
+    console.log("Event is triggered!");
+
+    console.log("Source: ", reviewPic.src);
+    console.log("Alt: ", reviewPic.alt);
+
+    let display = document.getElementById("image");
+    display.style.backgroundImage = `url('${reviewPic.src}')`;
+    display.textContent = reviewPic.alt;
+}
+
+function undo()
+{
+    let display = document.getElementById("image");
+    display.style.backgroundImage = "url('')";
+    display.textContent = "Hover over an image below to display here.";
+}
+
+document.querySelectorAll(".reviewPic").forEach(img => {
+    img.addEventListener("mouseover", function() {
+        upDate(this);
+    });
+        
+    img.addEventListener("mouseout", function() {
+        undo();
+    });
+});
